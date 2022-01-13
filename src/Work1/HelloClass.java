3b0131c4011f5d9455f3d05d7dd1cbdca2e9b6e6
DenysTrofimchuk <denyst89@gmail.com>
2022-01-12 21:22:34 -0500

    added even more work

--- src/Work1/HelloClass.java
+++ src/Work1/HelloClass.java
@@ -4,6 +4,7 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("WorkStarted");
         System.out.println("Created depository");
+        System.out.println("Made a few commits");
 
     }
 
