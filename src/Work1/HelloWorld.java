cf511389fa1ddb7115c0dfded7d13092603777a9
DenysTrofimchuk <denyst89@gmail.com>
2022-01-19 10:56:24 -0500

    day 3 git practice

--- src/Work1/HelloWorld.java
+++ src/Work1/HelloWorld.java
@@ -1,8 +1,7 @@
-package GitPractice;
+package Work1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello World");
     }
-
 }
